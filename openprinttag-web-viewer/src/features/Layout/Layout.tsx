1fc7a9110da0f05f99a499bcc66ec4a558468217
karelWeingart <karel.weingart@gmail.com>
2026-03-06 15:47:00 +0100

    remove references to deleted component

--- openprinttag-web-viewer/src/features/Layout/Layout.tsx
+++ openprinttag-web-viewer/src/features/Layout/Layout.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Layout.css";
-import { useTheme } from "../../context/ThemeContext";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
-  const { isDarkMode } = useTheme();
-  return <div className={`fluid-container  ${isDarkMode ? "dark-mode" : ""}`}>{children}</div>;
+  return <div className="fluid-container">{children}</div>;
 }
